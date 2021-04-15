45888d13a8e9536a3fe52a9c2fa808e23dd0fbec
Olivier Barais <olivieR.barais@gmail.com>
2021-04-15 16:14:27 +0200

    update

--- src/modules/blog/containers/dynamic-md/dynamic-md.component.ts
+++ src/modules/blog/containers/dynamic-md/dynamic-md.component.ts
@@ -1,14 +1,46 @@
-import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { Component, OnInit } from "@angular/core";
-import { DomSanitizer } from "@angular/platform-browser";
-import { ActivatedRoute, Router } from "@angular/router";
-import marked from "marked";
-import posts from "../../../../dynamicRoutes.json"
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { ActivatedRoute, Router } from '@angular/router';
+import marked from 'marked';
+import prism from 'prismjs';
+import 'prismjs/components/prism-bash';
+import 'prismjs/components/prism-css';
+import 'prismjs/components/prism-java';
+import 'prismjs/components/prism-javascript';
+import 'prismjs/components/prism-json';
+import 'prismjs/components/prism-jsx';
+import 'prismjs/components/prism-markup';
+// import 'prismjs/components/prism-visualbasic';
+
+import posts from '../../../../dynamicRoutes.json';
+
+// loadLanguages(['javascript', 'java', 'visualbasic', 'jsx', 'css', 'markup', 'bash', 'json']);
+
+marked.setOptions({
+    highlight(code, lang) {
+        console.log(lang)
+        console.log(prism.languages[lang])
+        if (prism.languages[lang]) {
+            console.log(prism.highlight(code, prism.languages[lang], lang));
+            return prism.highlight(code, prism.languages[lang], lang);
+        } else {
+            return code;
+        }
+    },
+    pedantic: false,
+    gfm: true,
+    breaks: false,
+    sanitize: true,
+    smartLists: true,
+    smartypants: false,
+    xhtml: false
+});
 
 @Component({
-    selector: "sb-dynamic-md",
-    templateUrl: "./dynamic-md.component.html",
-    styleUrls: ["./dynamic-md.component.scss"],
+    selector: 'sb-dynamic-md',
+    templateUrl: './dynamic-md.component.html',
+    styleUrls: ['./dynamic-md.component.scss'],
 })
 export class DynamicMdComponent implements OnInit {
     post!: any;
@@ -16,9 +48,9 @@ export class DynamicMdComponent implements OnInit {
     mdContent: any;
     private httpOptions = {
         headers: new HttpHeaders({
-            Accept: "text/html, application/xhtml+xml, */*",
+            Accept: 'text/html, application/xhtml+xml, */*',
         }),
-        responseType: "text" as "json",
+        responseType: 'text' as 'json',
     };
 
     constructor(
@@ -38,8 +70,9 @@ export class DynamicMdComponent implements OnInit {
             }
             if (this.post.mdsource != null) {
                 this.http
-                    .get(this.post.mdsource + "/download", this.httpOptions)
+                    .get(this.post.mdsource + '/download', this.httpOptions)
                     .subscribe((res) => {
+                            console.log( marked(res as string))
                         this.mdContent = this.sanitized.bypassSecurityTrustHtml(
                             marked(res as string)
                         );
